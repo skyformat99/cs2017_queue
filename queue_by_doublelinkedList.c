6a5a555564c182ae5d85bc110fabdbc7c8e49c92
ricktang <panda.twx@gmail.com>
2018-05-19 20:44:12 +0800

    [x] 设计数据结构(struct)

--- queue_by_doublelinkedList.c
+++ queue_by_doublelinkedList.c
@@ -3,6 +3,36 @@
 #include <string.h>  // 操作字符串需要的库函数
 
 
+// 设计数据结构
+struct node
+{
+    int val;
+    struct node* prev;
+    struct node* next;
+};
+typedef struct node Node;
+
+struct queue
+{
+    Node* head;
+    Node* tail;
+};
+typedef struct queue Queue;
+
+// void queue_print(Queue *q) //TODO 一会儿补写
+// {
+//     printf("\nqueue content: ");fflush(stdout);
+//     Node *cur = q->head->next;
+//     while (cur != q->tail)
+//     {
+//         printf("%d->", cur->val);fflush(stdout);
+//         cur = cur->next;
+//     }
+//     printf("NIL");
+//     printf("\n");fflush(stdout);
+// }
+
+
 void test_queue(){
     //server as test driver
     printf("This is the test function\n");
