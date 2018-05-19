80e0bd983cfb9e1bf719e293481e85a9388816c2
ricktang <panda.twx@gmail.com>
2018-05-19 20:26:55 +0800

    initial commit

---
+++ queue_by_doublelinkedList.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  // scanf/print/ 文件I/O
+#include <stdlib.h> // malloc
+#include <string.h>  // 操作字符串需要的库函数
+
+
+void test_queue(){
+    //server as test driver
+    printf("This is the test function\n");
+}
+
+int main(int argc, char* argv[]){
+    test_queue();
+    // queue_interaction();
+    return 0;
+}
